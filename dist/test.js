a42e769caf317ef0d1f5dfdadbaa936121c91f4e
Jean <acalto2@gmail.com>
2022-09-02 00:04:34 +0200

    all models broken

--- dist/test.js
+++ dist/test.js
@@ -1,5 +1,6 @@
+"use strict";
 /*import { BookStore } from "./models/book.js";
 const books = new BookStore ();
 const resul = books.index();
 console.log(resul);
-*/
+*/ 
--- src/test.js
+++
@@ -1,4 +0,0 @@
-import { Books } from "./models/book.js";
-var books = new Books();
-var resul = books.index();
-console.log(resul);
